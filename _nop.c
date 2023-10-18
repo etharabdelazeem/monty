93e1e6c4a6071b4583723c38f964291baf4da474
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-18 20:23:42 +0300

    Add nop function

---
+++ _nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - The opcode nop doesn’t do anything.
+ * @stack: the stack
+ * @linecount: line count
+ * Return: nothing
+ */
+void nop(stack_t **stack, unsigned int linecount)
+{
+  (void) stack;
+  (void) linecount;
+}
