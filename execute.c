21e2034e2b38b58a4cd78f88ba41f6c3b7fdd715
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-18 09:36:28 -0700

    added pint

--- execute.c
+++ execute.c
@@ -13,7 +13,7 @@ int execute(FILE *file, stack_t **stack, unsigned int count, char *line)
 {
 	instruction_t opset[] = {
 		{"push", push}, {"pall", pall},
-		/* {"pint", pint}, */
+		{"pint", pint}, 
 		{NULL, NULL}
 	};
 	unsigned int i = 0;
--- monty.h
+++ monty.h
@@ -62,5 +62,5 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void push_stack(stack_t **head);
 void freestack(stack_t *head);
-
+void pint(stack_t **stack, unsigned int linecount);
 #endif
