65ec7a98a04e1339b6b871f4a4c044273c538fe6
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-18 10:07:30 -0700

    added swap

--- execute.c
+++ execute.c
@@ -13,7 +13,7 @@ int execute(FILE *file, stack_t **stack, unsigned int count, char *line)
 {
 	instruction_t opset[] = {
 		{"push", push}, {"pall", pall},
-		{"pint", pint}, {"pop", pop}, 
+		{"pint", pint}, {"pop", pop}, {"swap", swap}, 
 		{NULL, NULL}
 	};
 	unsigned int i = 0;
